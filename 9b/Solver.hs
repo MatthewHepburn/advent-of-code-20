a226c930028513b6fc7feb094405705387d099a6
Matthew Hepburn <MatthewHepburn0@gmail.com>
2020-12-09 22:01:23 +0000

    Reshoogle to allow for quickcheck

--- 9b/Solver.hs
+++ 9b/Solver.hs
@@ -1,14 +1,17 @@
-import System.IO
+ {-# LANGUAGE TemplateHaskell #-}
+
+module Solver where
+
 import qualified Data.Map.Strict as Map
 import Text.Regex.PCRE
 import Text.Regex.Base.RegexLike
 import Data.List.Split
 import qualified Data.Set as Set
 import Data.Maybe
+import Test.QuickCheck
 
-main = do
-    handle <- openFile "input" ReadMode
-    fileContents <- hGetContents handle
+main:: String -> Int
+main fileContents = do
     let fileLines = filter (\x -> length x > 0) $ lines fileContents
     let numbers = map (\x -> read x :: Int) fileLines
 
@@ -21,11 +24,8 @@ main = do
     let regions = findRegions invalidNumber numbers []
 
     let longestRegion = foldl (\x y -> if length x > length y then x else y) [] regions
-    let solution = (maximum longestRegion) + (minimum longestRegion)
+    (maximum longestRegion) + (minimum longestRegion)
 
-    print solution
-
-    hClose handle
 
 solve:: [Int] -> [Int] -> Maybe Int
 solve _ [] = Nothing
@@ -47,9 +47,19 @@ findRegions target (x:xs) acc
         regionFromX = getRegion target (x:xs) []
         justRegion = fromJust regionFromX
 
+prop_RegionSum:: Int -> [Int] -> Bool
+prop_RegionSum target xs
+    | region == Nothing = True
+    | otherwise = (sum $ fromJust region) == target
+    where
+        region = getRegion target xs []
+
 getRegion:: Int -> [Int] -> [Int] -> Maybe [Int]
 getRegion 0 _ acc = Just acc
 getRegion _ [] acc = Nothing
 getRegion target (x:xs) acc
     | target < 0 = Nothing
     | otherwise = getRegion (target - x) xs (acc ++ [x])
+
+return []
+runTests = $quickCheckAll
---
+++ 9b/main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import qualified Solver as Solver
+import System.IO
+import System.Exit
+
+main = do
+    testResult <- Solver.runTests
+    if not testResult then die "Test failure" else putStrLn ""
+    handle <- openFile "input" ReadMode
+    fileContents <- hGetContents handle
+    let solution = Solver.main fileContents
+
+    print solution
+    hClose handle
