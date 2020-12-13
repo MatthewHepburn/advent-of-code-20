6adeab4c51a6a5d778f892d7c80ba1a9d0d3889d
Matthew Hepburn <MatthewHepburn0@gmail.com>
2020-12-13 15:20:04 +0000

    Cleanup 11

--- 11b/main.hs
+++ 11b/main.hs
@@ -4,12 +4,6 @@ import qualified Solver as Solver
 import System.IO
 import System.Exit
 
-findLast:: [Solver.Board] -> Solver.Board
-findLast (a:[]) = a
-findLast (a:b:cs)
-    | a == b = a
-    | otherwise = findLast (b:cs)
-
 main = do
     handle <- openFile "input" ReadMode
     fileContents <- hGetContents handle
